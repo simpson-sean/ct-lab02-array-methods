bc06e1d9c283c0a262abca97cc4bdea9e10f22e1
Sean <simpson.sean@outlook.com>
2021-07-20 17:11:39 -0700

    WORKDONE: Initial ACP

---
+++ __tests__/mapArrCalback-test.js
@@ -0,0 +1,9 @@
+//Test for map(arr, callback) function
+
+// import items
+
+describe('mappArrCallback', () => {
+    it('takes an array and callback and create new array with returned callback value', () => {
+        
+    })
+})
